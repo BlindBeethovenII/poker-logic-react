0d035d77ec18d362f04f2cb76143b63f8f2286a8
BlindBeethovenII <smbtesco@gmail.com>
2022-04-18 10:45:08 +0100

    git log along with script yarn add moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import PlayArea from './components/PlayArea';
+import GitInfo from './components/GitInfo';
 
 import { GameStateContextProvider } from './contexts/GameStateContext';
 
 const App = () => (
   <GameStateContextProvider>
     <PlayArea />
+    <GitInfo />
   </GameStateContextProvider>
 );
 
