865222d09098eb8d49ed6f4728780b283897c24c
BlindBeethovenII <smbtesco@gmail.com>
2022-05-31 10:26:32 +0100

    eslint now understands jest

---
+++ src/shared/generateHandOfHandType.test.js
@@ -0,0 +1,5 @@
+describe('generateHandOfHandType', () => {
+  it('will return a straight flush for HAND_TYPE_STRAIGHT_FLUSH for a new deck of cards', () => {
+    expect(true).toEqual(true);
+  });
+});
