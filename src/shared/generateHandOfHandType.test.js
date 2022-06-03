2d8ac65fce71fc4a3b9d83fd70bb7c8add445e94
BlindBeethovenII <smbtesco@gmail.com>
2022-06-03 15:08:17 +0100

    corrected corresponding test

--- src/shared/generateHandOfHandType.test.js
+++ src/shared/generateHandOfHandType.test.js
@@ -3,7 +3,7 @@ import {
   createNewDeck,
   createCard,
   calcHandType,
-  createSolutionHands,
+  createSolution,
 } from './card-functions';
 
 import {
@@ -188,10 +188,25 @@ describe('createSolutionHands', () => {
   it(`will return 4 hands for a consistent solution ${iterateN} times`, () => {
     for (let i = 0; i < iterateN; i += 1) {
       // get the hands of a solution
-      const solutionHands = createSolutionHands();
+      const { solutionHands } = createSolution();
 
       // there should be 4 of them
       expect(solutionHands.length).toEqual(4);
+
+      // note that createSolution() checks there are 20 unique cards in the solution
+
+      // check the hands are sorted by hand type, hightest first
+      const hand1Type = calcHandType(solutionHands[0]);
+      const hand2Type = calcHandType(solutionHands[1]);
+      const hand3Type = calcHandType(solutionHands[2]);
+      const hand4Type = calcHandType(solutionHands[3]);
+
+      expect(hand1Type > hand2Type
+        && hand1Type > hand3Type
+        && hand1Type > hand4Type
+        && hand2Type > hand3Type
+        && hand2Type > hand4Type
+        && hand3Type > hand4Type).toBeTruthy();
     }
   });
 });
